96d515f339427d0553d30f6b0592ef8dd01d2062
Florent Espanet <florent.esp@gmail.com>
2014-09-13 15:17:03 +0200

    Rename metwio to twiteo.

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup, find_packages
 from os.path import join, dirname
 
 setup(
-    name='metwio',
+    name='twiteo',
     version='0.1',
     packages=find_packages(),
     long_description=open(join(dirname(__file__), 'README.md')).read(),
     include_package_data=True,
     entry_points={
         'console_scripts': [
-            'metwio = metwio.metwio:main',
+            'twiteo = twiteo.twiteo:main',
         ]
         },
     install_requires = [
